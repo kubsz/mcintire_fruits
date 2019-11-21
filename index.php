62ac65e79addc7d623b4d7aa3089aa8f1f84dc48
Kubsz <me@kubs.uk>
2019-11-21 10:13:28 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>McIntire Fruit</title>
+    <link rel="stylesheet" type="text/css" href="/css/all.css">
+    <link rel="stylesheet" type="text/css" href="/css/nav.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600,800&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+<body>
+    <div class="nav">
+        <div class="inner-nav margin">
+            <div class="logo-container">
+                <div class="logo-left">MC</div>
+                <div class="logo-right">
+                    <span>Intire</span>
+                    <span>Fruit</span>
+                </div>
+            </div>
+            <a href="#" class="kids-area-button"><p>Kids Area </p></a>
+            <a href="#">Our Farms</a>
+            <a class="nav-dropdown-container" href="#">
+                <p>Vegetables</p>
+            </a>
+            <a href="#">Fruit</a>
+            <a href="#">Home</a>
+        </div>
+    </div>
+</body>
+</html>
+<script>
+    console.log("lol");
+</script>
